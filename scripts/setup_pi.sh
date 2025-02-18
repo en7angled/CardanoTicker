2b3db64f377ac8c03c075db63319cd614501df06
androclassic <ge0rgescu_andrei@yahoo.com>
2025-02-18 09:59:37 +0200

    small fix

--- scripts/setup_pi.sh
+++ scripts/setup_pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y python3 python3-pip gpiod libgpiod-dev
+sudo apt-get install -y python3 python3-pip gpiod libgpiod-dev libopenblas-dev
 sudo raspi-config nonint do_spi 0  # Enable SPI via raspi-config
 echo "Raspberry Pi setup complete."
